7ed43a641b6e3d1614b53000a44f6a99b1b3352e
Flora Santana <39812840+Floraluiza@users.noreply.github.com>
2021-10-25 00:51:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var idade
+
+
+
+idade = parseInt(prompt('Informe a sua idade'))
+
+
+
+//alert(idade)
+
+
+
+if(idade >= 16){
+
+
+
+  alert("Você pode votar!")
+
+
+
+}
+
+else{
+
+  alert("Você precisa esperar até completar 16 anos")
+
+}
